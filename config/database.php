cd71afa043a872ef3f6bd2a5eceec466140a655c
ROWEN SEBASTIAN JAMESRAJA <rowensebastian97@gmail.com>
2026-06-29 23:50:16 +0800

    HEE HEE

--- config/database.php
+++ config/database.php
@@ -84,23 +84,22 @@ return [
             ]) : [],
         ],
 
-     'pgsql' => [
+    'pgsql' => [
     'driver'   => 'pgsql',
-    'url'      => env('DB_URL'),
     'host'     => env('DB_HOST', '127.0.0.1'),
     'port'     => env('DB_PORT', '5432'),
-    'database' => env('DB_DATABASE', 'laravel'),
-    'username' => env('DB_USERNAME', 'root'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
     'password' => env('DB_PASSWORD', ''),
-    'charset'  => env('DB_CHARSET', 'utf8'),
+    'charset'  => 'utf8',
     'prefix'   => '',
-    'prefix_indexes' => true,
-    'search_path' => 'public',
+    'schema'   => 'public',
     'sslmode'  => env('DB_SSLMODE', 'require'),
     'options'  => [
-        PDO::ATTR_TIMEOUT => 5,
+        'sslrootcert' => '/etc/ssl/certs/DigiCertGlobalRootCA.pem',
     ],
 ],
+
 
 
         'sqlsrv' => [
